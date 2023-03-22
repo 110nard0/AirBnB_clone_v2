fcf392cbd3a6ffcdb02297129d6f7532bbd76713
psych0wh1z <ikeagudike@gmail.com>
2023-03-22 05:11:46 +0100

    Update docstrings

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This module defines a class to manage file storage for hbnb clone"""
+"""This module defines a class to manage file storage for AirBnB clone"""
 import json
 from models.base_model import BaseModel
 from models.user import User
@@ -17,7 +17,7 @@ classes = {
 
 
 class FileStorage:
-    """This class manages storage of hbnb models in JSON format"""
+    """This class manages storage of AirBnB models in JSON format"""
     __file_path = 'file.json'
     __objects = {}
 
