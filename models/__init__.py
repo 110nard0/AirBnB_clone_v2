1dd95bbd75ae839eb1fe94036de3009839e43bf3
psych0wh1z <ikeagudike@gmail.com>
2023-03-24 06:26:18 +0100

    Add all model classes for easy import

--- models/__init__.py
+++ models/__init__.py
@@ -4,6 +4,13 @@ This module creates an instance object of FileStorage or DBStorage type
 and calls the reload method to retrieve saved model class instances
 """
 from os import environ
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
 
 if environ.get('HBNB_TYPE_STORAGE') == 'db':
