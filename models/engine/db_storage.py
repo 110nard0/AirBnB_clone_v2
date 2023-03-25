062111f4ca163fe1131c118bfe557fa23b6c6f54
psych0wh1z <ikeagudike@gmail.com>
2023-03-25 11:58:26 +0100

    Remove _sa_instance_attr from model objects

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -50,10 +50,9 @@ class DBStorage():
                 for obj in self.__session.query(val).all():
                     obj_dict[obj.__class__.__name__ + '.' + obj.id] = obj
 
-        if getenv('HBNB_TYPE_STORAGE') != 'db':
-            if obj_dict:
-                for v in obj_dict.values():
-                    v.__dict__.pop('_sa_instance_state', None)
+        if obj_dict:
+            for v in obj_dict.values():
+                v.__dict__.pop('_sa_instance_state', None)
         return obj_dict
 
     def new(self, obj):
