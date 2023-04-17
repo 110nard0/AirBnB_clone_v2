29cbe8997ba02bfb91a7b7fcc777cdbe98623a2a
psych0wh1z <ikeagudike@gmail.com>
2023-04-17 10:52:25 +0100

    Test improved script

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 # sets up web servers for the deployment of web_static
 
-static_string="server_name _;\n\n\tlocation \/hbnb_static {\n\t\talias  \/data\/web_static\/current\/;\n\t}"
-
 # update environment and install nginx
-sudo apt update
+sudo apt-get -y update
+sudo apt-get -y install nginx
 sudo apt install -y nginx
 sudo ufw allow 'Nginx HTTP'
+sudo service nginx start
 
 # create static content folders and create test webpage
-sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
 echo "Hello World!" | sudo tee /data/web_static/releases/test/index.html > /dev/null
 
 # create symbolic link to static folders
@@ -20,7 +20,7 @@ sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 
 # edit nginx configuration file in place
-sudo sed -i "s/server_name _;/$static_string/" /etc/nginx/sites-available/default
+sudo sed -i '44i \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
 
 # restart nginx server
 sudo service nginx restart
