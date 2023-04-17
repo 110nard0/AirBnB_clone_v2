72aaa5cb983c8c85a0e7bd88373742c7042cee61
psych0wh1z <ikeagudike@gmail.com>
2023-04-17 11:36:45 +0100

    Tweak script

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,6 +4,7 @@ web_static folder in AirBnB Clone repository
 and distributes it to connected web servers
 """
 from fabric.api import *
+from datetime import datetime
 from os import path
 
 
@@ -22,40 +23,40 @@ def do_deploy(archive_path):
         True (script works) or False (otherwise)
     """
     try:
-        if not path.exists(archive_path):
+        if not (path.exists(archive_path)):
             return False
-        else:
-            # upload archive
-            put(archive_path, '/tmp/')
 
-            # create target directory
-            created_at = archive_path[-18:-4]
-            run('sudo mkdir -p /data/web_static/\
-                releases/web_static_{}/'.format(created_at))
+        # upload archive
+        put(archive_path, '/tmp/')
 
-            # uncompress archive to target directory
-            run('sudo tar -xzf /tmp/web_static_{}.tgz -C \
-                /data/web_static/releases/web_static_{}/'
-                .format(created_at, created_at))
+        # create target directory
+        created_at = archive_path[-18:-4]
+        run('sudo mkdir -p /data/web_static/\
+            releases/web_static_{}/'.format(created_at))
 
-            # delete archive
-            run('sudo rm /tmp/web_static_{}.tgz'.format(created_at))
+        # uncompress archive to target directory
+        run('sudo tar -xzf /tmp/web_static_{}.tgz -C \
+            /data/web_static/releases/web_static_{}/'.
+            format(created_at, created_at))
 
-            # move web_static content into hosting directory
-            run('sudo mv /data/web_static/releases/web_static_{}/web_static/* \
-                /data/web_static/releases/web_static_{}/'.
-                format(created_at, created_at))
+        # delete archive
+        run('sudo rm /tmp/web_static_{}.tgz'.format(created_at))
 
-            # remove web_static sub-directory
-            run('sudo rm -rf /data/web_static/releases/\
-                web_static_{}/web_static'.format(created_at))
+        # move web_static content into hosting directory
+        run('sudo mv /data/web_static/releases/web_static_{}/web_static/* \
+            /data/web_static/releases/web_static_{}/'.
+            format(created_at, created_at))
 
-            # delete pre-existing symbolic link
-            run('sudo rm -rf /data/web_static/current')
+        # remove web_static sub-directory
+        run('sudo rm -rf /data/web_static/releases/\
+            web_static_{}/web_static'.format(created_at))
 
-            # create new symbolic link to archive content
-            run('sudo ln -s /data/web_static/releases/\
-                web_static_{}/ /data/web_static/current'.format(created_at))
+        # delete pre-existing symbolic link
+        run('sudo rm -rf /data/web_static/current')
+
+        # create new symbolic link to archive content
+        run('sudo ln -s /data/web_static/releases/\
+            web_static_{}/ /data/web_static/current'.format(created_at))
     except Exception as e:
         return False
 
