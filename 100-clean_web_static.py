adb4da64b60daa736cb22eb75166bbe6532d361a
psych0wh1z <ikeagudike@gmail.com>
2023-04-17 15:51:25 +0100

    Modify script

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Deletes out-of-date web_static archives
 """
-from fabric.api import env, cd, lcd, run
+from fabric.api import env, cd, run
 import os
 
 env.hosts = ['54.152.106.255', '100.25.181.181']
@@ -16,7 +16,7 @@ def do_clean(number=0):
     """
     number = 1 if int(number) == 0 else int(number)
 
-    with lcd('./versions'):
+    with cd.local('./versions'):
         local("ls -lt | tail -n +{} | rev | cut -f1 -d" " | rev | \
               xargs -d '\n' rm".format(1 + number))
     with cd('/data/web_static/releases/'):
