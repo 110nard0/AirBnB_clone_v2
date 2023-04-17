e4819dfe0ca700325b580a02181a39cd94dee335
psych0wh1z <ikeagudike@gmail.com>
2023-04-17 11:57:24 +0100

    Fix pycodestyle errors

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,9 +3,8 @@
 web_static folder in AirBnB Clone repository
 and distributes it to connected web servers
 """
-from fabric.api import env, put, run 
+from fabric.api import env, put, run
 impost os.path
-
 
 env.hosts = ['54.152.106.255', '100.25.181.181']
 env.user = 'ubuntu'
