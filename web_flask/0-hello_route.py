44bf28ee2d69daf383c0e11c240a8d4441401874
psych0wh1z <ikeagudike@gmail.com>
2023-04-25 12:22:20 +0100

    Fix string format

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,7 +6,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes = False)
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
     """Return query string
     """
@@ -14,5 +14,5 @@ def hello_hbnb():
 
 
 if __name__ == "__main__":
-#   app.url_map.strict_slashes = False
+    # app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
