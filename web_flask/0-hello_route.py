9ee2db1ad4af1a3ed264a9a060ac45e5605fcf81
psych0wh1z <ikeagudike@gmail.com>
2023-04-25 13:45:33 +0100

    Test new output

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,13 +6,13 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def hello_hbnb():
     """Return query string
     """
     return "Hello, HBNB!"
 
 
-if __name__ == "__main__":
-    # app.url_map.strict_slashes = False
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
