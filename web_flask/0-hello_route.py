a724972a4ee359806d02b7979c598fc51f88339b
psych0wh1z <ikeagudike@gmail.com>
2023-04-25 12:20:03 +0100

    Test default argument

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-"""
-starts a Flask web application
+"""Start a Flask web application
 """
 
 from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', strict_slashes = False)
 def hello_hbnb():
-    """Starts a web server
+    """Return query string
     """
     return "Hello, HBNB!"
 
 
 if __name__ == "__main__":
-    app.url_map.strict_slashes = False
+#   app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
