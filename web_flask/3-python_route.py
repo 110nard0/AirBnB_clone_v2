2b2e25c23b6040e5ea318741732422f22a903275
psych0wh1z <ikeagudike@gmail.com>
2023-04-25 14:48:26 +0100

    Move strict_slash clause to starter code

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -7,34 +7,35 @@ from markupsafe import escape
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def hello():
     """Returns query string
     """
     return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route('/hbnb')
 def display():
     """Displays query string
     """
     return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
+@app.route('/c/<text>')
 def route_c(text):
     """Displays a text based on input URI
     """
     return "C {}".format(escape(text.replace('_', ' ')))
 
 
-@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def route_python(text="is cool"):
+@app.route('/python/')
+@app.route('/python/<text>')
+def route_python(text='is cool'):
     """Displays a text based on input URI with default value present
     """
     return "Python {}".format(escape(text.replace('_', ' ')))
 
 
 if __name__ == '__main__':
+    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
