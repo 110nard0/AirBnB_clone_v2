f2e92e2974e6497fecac18744f6ea8dcbc045df5
psych0wh1z <ikeagudike@gmail.com>
2023-04-25 14:01:52 +0100

    Fix return string

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,13 +6,12 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
     """Return query string
     """
-    return "Hello, HBNB!"
+    return "Hello HBNB!"
 
 
 if __name__ == '__main__':
-    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
