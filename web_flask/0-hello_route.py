8fbd072ff777b0045f6dfbd3bf2639ecb1d509a5
psych0wh1z <ikeagudike@gmail.com>
2023-04-25 12:15:26 +0100

    Change script to match target flask version

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,7 +7,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def hello_hbnb():
     """Starts a web server
     """
@@ -15,4 +15,5 @@ def hello_hbnb():
 
 
 if __name__ == "__main__":
+    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
