78414b99db6194c9e0ddcbe67dec2be9c93aba6f
psych0wh1z <ikeagudike@gmail.com>
2023-04-28 00:40:19 +0100

    Create jinja template for all states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Starts a Flask web application that fetches data from storage engine
+"""
+
+from models import storage
+from flask import Flask, render_template
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route('/states_list')
+def states():
+    """Displays a HTML page with a list of all states in current db session 
+    """
+    states = storage.all('State')
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Removes the current SQLAlchemy Session:
+    """
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
