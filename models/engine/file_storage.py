9153d1f6d68032ea95e3e5d0b85d2dfaf8a1074b
psych0wh1z <ikeagudike@gmail.com>
2023-04-27 22:44:23 +0100

    Change string formatting

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -40,7 +40,7 @@ class FileStorage:
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
-        key = f"{obj.__class__.__name__}.{obj.id}"
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
 
     def save(self):
